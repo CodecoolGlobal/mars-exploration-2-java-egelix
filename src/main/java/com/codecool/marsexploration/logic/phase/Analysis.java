0b014c9345ffd5d373f70b0edda6a0662e96a660
ErydCode <dyre.espana@gmail.com>
2023-05-22 22:35:54 +0200

    finished serviceClass Analysis

--- src/main/java/com/codecool/marsexploration/logic/phase/Analysis.java
+++ src/main/java/com/codecool/marsexploration/logic/phase/Analysis.java
@@ -3,6 +3,7 @@ package com.codecool.marsexploration.logic.phase;
 import com.codecool.marsexploration.data.Context;
 import com.codecool.marsexploration.data.Coordinate;
 import com.codecool.marsexploration.data.Symbol;
+import com.codecool.marsexploration.data.rover.FoundResource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -14,7 +15,19 @@ public class Analysis implements Phase {
         Set<Coordinate> scannedCoordinates = context.getScannedFields();
         String[][] map = context.getMap();
         getPossibleNextCoordinates(context, scannedCoordinates, map);
-        //ToDo make add FoundResources to Set and keep old set!
+        getFoundResources(context, scannedCoordinates, map);
+    }
+
+    private void getFoundResources(Context context, Set<Coordinate> scannedCoordinates, String[][] map) {
+        Set<FoundResource> foundResources = new HashSet<>();
+        for (Coordinate coordinate : scannedCoordinates) {
+            if (map[coordinate.y()][coordinate.x()].equals(Symbol.MINERAL.getSymbol()) ||
+                    map[coordinate.y()][coordinate.x()].equals(Symbol.WATER.getSymbol())) {
+                FoundResource foundResource = new FoundResource(map[coordinate.y()][coordinate.x()], coordinate);
+                foundResources.add(foundResource);
+            }
+        }
+        context.setFoundResources(foundResources);
     }
 
     private void getPossibleNextCoordinates(Context context, Set<Coordinate> scannedCoordinates, String[][] map) {
