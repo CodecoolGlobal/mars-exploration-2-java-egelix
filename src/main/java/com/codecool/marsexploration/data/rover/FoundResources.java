d12fc6fa311084774a7ecb93d2a1b38fc9f236bb
ErydCode <dyre.espana@gmail.com>
2023-05-22 14:21:03 +0200

    add FoundResources Record for Set Field Context

---
+++ src/main/java/com/codecool/marsexploration/data/rover/FoundResources.java
@@ -0,0 +1,7 @@
+package com.codecool.marsexploration.data.rover;
+
+import com.codecool.marsexploration.data.Coordinate;
+import com.codecool.marsexploration.data.Symbol;
+
+public record FoundResources(Symbol resource, Coordinate coordinate) {
+}
