1564177f29ab539756fa5ca5c4dd7103c7d119b4
ErydCode <dyre.espana@gmail.com>
2023-05-22 16:27:34 +0200

    cleanCode add getter setter and fields. 2D Array is not int is String

--- src/main/java/com/codecool/marsexploration/data/Context.java
+++ src/main/java/com/codecool/marsexploration/data/Context.java
@@ -8,33 +8,37 @@ import java.util.Set;
 
 public class Context {
     private final int timeout;
-    private final int[][] map;
+    private final String[][] map;
     private final Coordinate landing;
     private final Rover rover;
-
-    public int getStepNumber() {
-        return stepNumber;
-    }
-
     private int stepNumber;
     private Outcome outcome;
     private List<LogFile> logFile;
+    private Set<Coordinate> scannedFields;
     private Set<Coordinate> possibleNextMove;
     private Set<FoundResources> foundResources;
 
-    public Context(int timeout, int[][] map, Coordinate landing, Rover rover) {
+    public Context(int timeout, String[][] map, Coordinate landing, Rover rover) {
         this.timeout = timeout;
         this.map = map;
         this.landing = landing;
         this.rover = rover;
     }
 
-    public void setOutcome(Outcome outcome) {
-        this.outcome = outcome;
-    }
-
     public int getTimeout() {
         return timeout;
+    }
+
+    public String[][] getMap() {
+        return map;
+    }
+
+    public Coordinate getLanding() {
+        return landing;
+    }
+
+    public Rover getRover() {
+        return rover;
     }
 
     public int getStepNumber() {
@@ -45,12 +49,28 @@ public class Context {
         this.stepNumber = stepNumber;
     }
 
+    public Outcome getOutcome() {
+        return outcome;
+    }
+
+    public void setOutcome(Outcome outcome) {
+        this.outcome = outcome;
+    }
+
     public List<LogFile> getLogFile() {
         return logFile;
     }
 
     public void setLogFile(List<LogFile> logFile) {
         this.logFile = logFile;
+    }
+
+    public Set<Coordinate> getScannedFields() {
+        return scannedFields;
+    }
+
+    public void setScannedFields(Set<Coordinate> scannedFields) {
+        this.scannedFields = scannedFields;
     }
 
     public Set<Coordinate> getPossibleNextMove() {
