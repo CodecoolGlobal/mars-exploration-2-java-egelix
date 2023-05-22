81c36803626e876db45c836a84b3f15dd29f6a90
ErydCode <dyre.espana@gmail.com>
2023-05-23 00:13:55 +0200

    rework Analysis = DRYS add Methode for Outcome.WRONG_LANDING_COORDINATES

--- src/main/java/com/codecool/marsexploration/logic/phase/Analysis.java
+++ src/main/java/com/codecool/marsexploration/logic/phase/Analysis.java
@@ -2,6 +2,7 @@ package com.codecool.marsexploration.logic.phase;
 
 import com.codecool.marsexploration.data.Context;
 import com.codecool.marsexploration.data.Coordinate;
+import com.codecool.marsexploration.data.Outcome;
 import com.codecool.marsexploration.data.Symbol;
 import com.codecool.marsexploration.data.rover.FoundResource;
 
@@ -9,34 +10,45 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Analysis implements Phase {
-
     @Override
     public void perform(Context context) {
         Set<Coordinate> scannedCoordinates = context.getScannedFields();
         String[][] map = context.getMap();
-        getPossibleNextCoordinates(context, scannedCoordinates, map);
-        getFoundResources(context, scannedCoordinates, map);
+        Set<Coordinate> possibleNextMove = new HashSet<>();
+        Set<FoundResource> foundResources = new HashSet<>();
+        int counterForOutcome = 0;
+        for (Coordinate coordinate : scannedCoordinates) {
+            String symbol = map[coordinate.y()][coordinate.x()];
+            addPossibleNextMove(context, possibleNextMove, coordinate, symbol);
+            addFoundResources(context, foundResources, coordinate, symbol);
+            counterForOutcome = getCounter(counterForOutcome, symbol);
+        }
+        if (counterForOutcome >= 8) {
+            context.setOutcome(Outcome.WRONG_LANDING_COORDINATES);
+        }
     }
 
-    private void getFoundResources(Context context, Set<Coordinate> scannedCoordinates, String[][] map) {
-        Set<FoundResource> foundResources = new HashSet<>();
-        for (Coordinate coordinate : scannedCoordinates) {
-            if (map[coordinate.y()][coordinate.x()].equals(Symbol.MINERAL.getSymbol()) ||
-                    map[coordinate.y()][coordinate.x()].equals(Symbol.WATER.getSymbol())) {
-                FoundResource foundResource = new FoundResource(map[coordinate.y()][coordinate.x()], coordinate);
-                foundResources.add(foundResource);
-            }
+    private void addPossibleNextMove(Context context, Set<Coordinate> possibleNextMove, Coordinate coordinate, String symbol) {
+        if (symbol.equals(Symbol.EMPTY.getSymbol())) {
+            possibleNextMove.add(coordinate);
+        }
+        context.setPossibleNextMove(possibleNextMove);
+    }
+
+    private void addFoundResources(Context context, Set<FoundResource> foundResources, Coordinate coordinate, String symbol) {
+        if (symbol.equals(Symbol.MINERAL.getSymbol()) ||
+                symbol.equals(Symbol.WATER.getSymbol())) {
+            FoundResource foundResource = new FoundResource(symbol, coordinate);
+            foundResources.add(foundResource);
         }
         context.setFoundResources(foundResources);
     }
 
-    private void getPossibleNextCoordinates(Context context, Set<Coordinate> scannedCoordinates, String[][] map) {
-        Set<Coordinate> possibleNextMove = new HashSet<>();
-        for (Coordinate coordinate : scannedCoordinates) {
-            if (map[coordinate.y()][coordinate.x()].equals(Symbol.EMPTY.getSymbol())) {
-                possibleNextMove.add(coordinate);
-            }
+    private int getCounter(int counterForOutcome, String symbol) {
+        if (symbol.equals(Symbol.PIT.getSymbol()) ||
+                symbol.equals(Symbol.MOUNTAIN.getSymbol())) {
+            counterForOutcome++;
         }
-        context.setPossibleNextMove(possibleNextMove);
+        return counterForOutcome;
     }
 }
