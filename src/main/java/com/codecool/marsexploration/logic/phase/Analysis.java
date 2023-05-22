2bc710aaa75e204feb21d915ee38462fb8dd98e9
ErydCode <dyre.espana@gmail.com>
2023-05-22 12:10:02 +0200

    add 5 Phase Classes

---
+++ src/main/java/com/codecool/marsexploration/logic/phase/Analysis.java
@@ -0,0 +1,10 @@
+package com.codecool.marsexploration.logic.phase;
+
+import com.codecool.marsexploration.data.Context;
+
+public class Analysis implements Phase {
+    @Override
+    public void perform(Context context) {
+
+    }
+}
