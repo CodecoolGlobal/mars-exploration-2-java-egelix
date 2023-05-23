7dd5838cb51f55c01d3e719f7ddc71107af05d96
Michael <knab.codecool@gmail.com>
2023-05-23 14:01:33 +0200

    started Movement logic structure

--- src/main/java/com/codecool/marsexploration/logic/phase/Movement.java
+++ src/main/java/com/codecool/marsexploration/logic/phase/Movement.java
@@ -1,10 +1,51 @@
 package com.codecool.marsexploration.logic.phase;
 
 import com.codecool.marsexploration.data.Context;
+import com.codecool.marsexploration.data.Coordinate;
+
+import java.util.Set;
 
 public class Movement implements Phase{
+    //get coordinates of landing, get surrounding fields
+
+    //TODO: random movement
+    //and choose randomly, count resources and save coordinates
+
+    //TODO: return movement
+    //return after an outcome is met
+
+    //TODO: sequential movement
+    //move from top to bottom row after row
+
+    //TODO: smartMovement
+    //and filter for
+    //resources(, save coordinates) and count for condition (water & minerals)
+
     @Override
     public void perform(Context context) {
+        Coordinate landing = context.getLanding();
+        Set<Coordinate> scannedFields = context.getScannedFields();
+
+        //TODO: implement logic for switching between movement approach/return movement
+
+        //eventually return new map with resources
+    }
+
+    private void randomMovement(Set<Coordinate> scannedFields) {
+        for (Coordinate coordinate : scannedFields) {
+            System.out.println(coordinate);
+        }
+    }
+
+    private void returnHome () {
+
+    }
+
+    private void moveSequentially () {
+
+    }
+
+    private void moveSmart () {
 
     }
 }
