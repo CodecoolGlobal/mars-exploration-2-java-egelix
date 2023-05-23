a3ae3ab8ffa1259a7b0dc2201a1e6803f569f102
ErydCode <dyre.espana@gmail.com>
2023-05-23 17:00:24 +0200

    rework Application remove List<Analyzer> remove not needed imports

--- src/main/java/com/codecool/marsexploration/Application.java
+++ src/main/java/com/codecool/marsexploration/Application.java
@@ -3,18 +3,9 @@ package com.codecool.marsexploration;
 import com.codecool.marsexploration.data.Coordinate;
 import com.codecool.marsexploration.data.SimulationInput;
 import com.codecool.marsexploration.logic.ExplorationSimulator;
-import com.codecool.marsexploration.logic.analyzer.Analyzer;
-import com.codecool.marsexploration.logic.analyzer.LackOfRessourcesAnalyzer;
-import com.codecool.marsexploration.logic.analyzer.SuccessAnalyzer;
-import com.codecool.marsexploration.logic.analyzer.TimeoutAnalyzer;
-
-import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-
-        List<Analyzer> analyzers = List.of(new TimeoutAnalyzer(), new SuccessAnalyzer(), new LackOfRessourcesAnalyzer());
-
         ExplorationSimulator simulator = new ExplorationSimulator();
         SimulationInput input = new SimulationInput(
                 "src/main/resources/exploration-1.map",
