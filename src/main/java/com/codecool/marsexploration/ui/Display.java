4adb73112c1b5999e52c2b6acbe36beb08326988
ErydCode <dyre.espana@gmail.com>
2023-05-23 17:02:17 +0200

    add 2 new Methods to Display

--- src/main/java/com/codecool/marsexploration/ui/Display.java
+++ src/main/java/com/codecool/marsexploration/ui/Display.java
@@ -3,13 +3,27 @@ package com.codecool.marsexploration.ui;
 public class Display {
     private static final String ANSI_RED = "\u001B[31m";
     private static final String ANSI_DEFAULT = "\u001B[0m";
+    private final int displayLength = 80;
+
+    public void printTitle(String title) {
+        System.out.println();
+        String fillWithSpace = " ".repeat((displayLength - title.length()) / 2);
+        System.out.println("✧".repeat(displayLength));
+        System.out.println(fillWithSpace + title.toUpperCase() + fillWithSpace);
+        System.out.println("✧".repeat(displayLength));
+    }
+
+    public void printSubtitle(String subtitle) {
+        String fillWithSpace = " ".repeat((displayLength - subtitle.length()) / 2);
+        System.out.println(fillWithSpace + subtitle.toUpperCase() + fillWithSpace);
+        System.out.println("✧".repeat(displayLength));
+    }
 
     public void message(String message) {
         System.out.println(message);
     }
 
     public void printEndLines() {
-        int displayLength = 80;
         System.out.println("✧".repeat(displayLength));
     }
 
