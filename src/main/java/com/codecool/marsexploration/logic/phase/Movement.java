e520a3900aa6ad6b9e95b64bc4f117bbc3f2d9be
ErydCode <dyre.espana@gmail.com>
2023-05-24 00:25:38 +0200

    Add Constructor for Move chosenMove

--- src/main/java/com/codecool/marsexploration/logic/phase/Movement.java
+++ src/main/java/com/codecool/marsexploration/logic/phase/Movement.java
@@ -3,24 +3,25 @@ package com.codecool.marsexploration.logic.phase;
 import com.codecool.marsexploration.data.Context;
 import com.codecool.marsexploration.data.Coordinate;
 import com.codecool.marsexploration.data.rover.Rover;
-import com.codecool.marsexploration.logic.movement.MoveInterface;
+import com.codecool.marsexploration.logic.movement.Move;
 import com.codecool.marsexploration.logic.movement.RandomMove;
 
 import java.util.Set;
 
-public class Movement implements Phase{
-    MoveInterface chosenMove;
+public class Movement implements Phase {
+    private final Move chosenMove;
+
+    public Movement(Move chosenMove) {
+        this.chosenMove = chosenMove;
+    }
 
     @Override
     public void perform(Context context) {
         Rover rover = context.getRover();
-        String [][] map = context.getMap();
+        String[][] map = context.getMap();
         Coordinate landing = context.getLanding();
-        Set<Coordinate> scannedFields = context.getScannedFields();
-        chosenMove = new RandomMove();
+        Set<Coordinate> scannedFields = context.getNextMoveCoordinates();
         //TODO: implement logic for switching between movement approach/return movement
         chosenMove.move(map, landing, scannedFields, rover);
-
     }
-
 }
