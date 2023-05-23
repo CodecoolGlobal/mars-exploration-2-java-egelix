a9531cf4edfcf368e9188ddb395be43813188ead
ErydCode <dyre.espana@gmail.com>
2023-05-23 22:51:44 +0200

    return null to make main test

--- src/main/java/com/codecool/marsexploration/utility/MapGenerator.java
+++ src/main/java/com/codecool/marsexploration/utility/MapGenerator.java
@@ -3,6 +3,7 @@ package com.codecool.marsexploration.utility;
 public class MapGenerator {
     public String[][] generate(String mapPath) {
         //TODO: write map generator method
-        return new String[][];
+        //return new String[][];
+        return null;
     }
 }
