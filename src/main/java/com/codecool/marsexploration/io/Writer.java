0f37dcb7ef2b7561bb6ceec527978a476b5c6c94
Martin Egert <m.egert82@gmail.com>
2023-05-23 22:17:17 +0200

    added Writer for Filelogs + Test

--- src/main/java/com/codecool/marsexploration/io/Writer.java
+++ src/main/java/com/codecool/marsexploration/io/Writer.java
@@ -1,4 +1,41 @@
 package com.codecool.marsexploration.io;
 
+import com.codecool.marsexploration.data.Coordinate;
+import com.codecool.marsexploration.data.LogFile;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+
 public class Writer {
+    //File path = new File(System.getProperty("src/main/resources/output/"));
+    //File file = new File(path,"Logfile.log");
+    public void writeLog(LogFile log) {
+        try {
+            doWrite(log);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void doWrite(LogFile log) throws IOException {
+        String path = "src/main/resources/output/";
+        String fileName = path + "Logfile.log";
+        String logStr = getLogStr(log);
+        File file = new File(fileName);
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
+        writer.write(logStr);
+        writer.close();
+    }
+
+    public String getLogStr(LogFile log) {
+        String logStr = "STEP " + log.step() +
+                "; EVENT " + log.event() +
+                "; UNIT rover-" + log.roverId() +
+                "; POSITION [" + log.position().y() +
+                "," + log.position().x() + "]";
+        return logStr;
+    }
 }
--- src/test/java/com/codecool/marsexploration/io/WriterTest.java
+++ src/test/java/com/codecool/marsexploration/io/WriterTest.java
@@ -1,6 +1,37 @@
 package com.codecool.marsexploration.io;
 
+import com.codecool.marsexploration.data.Coordinate;
+import com.codecool.marsexploration.data.LogFile;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class WriterTest {
-  
+    Writer writer = new Writer();
+
+    public static Stream<Arguments> argumentsForWriterTests() {
+        return Stream.of(
+                Arguments.of(new LogFile(3, "stepping", 4, new Coordinate(3,3), "unknown"),
+                        "STEP 3; EVENT stepping; UNIT rover-4; POSITION [3,3]")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("argumentsForWriterTests")
+    void writeLogTest(LogFile log, String expected) {
+        writer.writeLog(log);
+        assertEquals(expected, expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("argumentsForWriterTests")
+    void getLogStringTest(LogFile log, String expected) {
+        String result = writer.getLogStr(log);
+        assertEquals(result, expected);
+    }
 }
