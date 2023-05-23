9ff0f810d7dc82fde15f8a15cf824eb0ea2c5406
ErydCode <dyre.espana@gmail.com>
2023-05-23 13:29:43 +0200

    add dataClass FoundResource.java

--- src/main/java/com/codecool/marsexploration/data/rover/FoundResource.java
+++ src/main/java/com/codecool/marsexploration/data/rover/FoundResource.java
@@ -2,5 +2,40 @@ package com.codecool.marsexploration.data.rover;
 
 import com.codecool.marsexploration.data.Coordinate;
 
-public record FoundResource(String resource, Coordinate coordinate) {
+import java.util.Set;
+
+public class FoundResource {
+    private final String symbol;
+    private int amount;
+    private Set<Coordinate> coordinate;
+
+    public FoundResource(String symbol, int amount, Set<Coordinate> coordinate) {
+        this.symbol = symbol;
+        this.amount = amount;
+        this.coordinate = coordinate;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public Set<Coordinate> getCoordinate() {
+        return coordinate;
+    }
+
+    public void setCoordinate(Set<Coordinate> coordinate) {
+        this.coordinate = coordinate;
+    }
+
+    public void addCoordinate(Coordinate coordinate) {
+        this.coordinate.add(coordinate);
+    }
 }
