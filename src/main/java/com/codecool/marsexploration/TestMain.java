cd066d429936d082aecee88e536f1e53da43e872
ErydCode <dyre.espana@gmail.com>
2023-05-24 11:28:11 +0200

    new Test of CoordinateCreator to see what happen if start is 0, 0

--- src/main/java/com/codecool/marsexploration/TestMain.java
+++ src/main/java/com/codecool/marsexploration/TestMain.java
@@ -9,7 +9,7 @@ public class TestMain {
 
     public static void main(String[] args) {
         CoordinateCreator coordinateCreator = new CoordinateCreator();
-        Coordinate coordinateTest = new Coordinate(10, 10);
+        Coordinate coordinateTest = new Coordinate(0, 0);
         Set<Coordinate> test = coordinateCreator.aroundRover(coordinateTest, 20, 2);
         System.out.println(test.size());
         for (Coordinate c : test) {
