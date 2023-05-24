d6840906c60d815e41a35344158b28bac5f2104a
ErydCode <dyre.espana@gmail.com>
2023-05-24 13:10:35 +0200

    add getName for Log EVENT

--- src/main/java/com/codecool/marsexploration/logic/movement/ReturnTeleport.java
+++ src/main/java/com/codecool/marsexploration/logic/movement/ReturnTeleport.java
@@ -6,8 +6,19 @@ import com.codecool.marsexploration.data.rover.Rover;
 import java.util.Set;
 
 public class ReturnTeleport implements Move{
+    private final String name;
+
+    public ReturnTeleport(String name) {
+        this.name = name;
+    }
+
     @Override
     public void move(String[][] map, Coordinate landing, Set<Coordinate> scannedFields, Rover rover) {
         rover.setPosition(landing);
+    }
+
+    @Override
+    public String getName() {
+        return name;
     }
 }
