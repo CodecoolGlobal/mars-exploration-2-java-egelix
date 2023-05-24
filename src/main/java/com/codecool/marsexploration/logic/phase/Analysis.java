c2bcd9018152ffbaee9890867051bbb77fc28278
Martin Egert <m.egert82@gmail.com>
2023-05-24 22:29:16 +0200

    redone after merge

--- src/main/java/com/codecool/marsexploration/logic/phase/Analysis.java
+++ src/main/java/com/codecool/marsexploration/logic/phase/Analysis.java
@@ -1,9 +1,12 @@
 package com.codecool.marsexploration.logic.phase;
 
 import com.codecool.marsexploration.data.Context;
+import com.codecool.marsexploration.data.Outcome;
 import com.codecool.marsexploration.logic.analyzer.Analyzer;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 public class Analysis implements Phase {
 
