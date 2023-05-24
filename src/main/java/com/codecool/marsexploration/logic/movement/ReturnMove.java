7b1c0df487f1455ef1b3524d6db3837d5373c30e
ErydCode <dyre.espana@gmail.com>
2023-05-24 16:57:32 +0200

    Implement Move to ReturnMove

--- src/main/java/com/codecool/marsexploration/logic/movement/ReturnMove.java
+++ src/main/java/com/codecool/marsexploration/logic/movement/ReturnMove.java
@@ -1,6 +1,16 @@
 package com.codecool.marsexploration.logic.movement;
 
-public class ReturnMove {
-    //TODO: return movement
-    //return after an outcome is met
+import com.codecool.marsexploration.data.Context;
+
+public class ReturnMove implements Move {
+    @Override
+    public void move(Context context) {
+    //TODO: return movement return after an outcome is met
+
+    }
+
+    @Override
+    public String getName() {
+        return "Return to Space Ship";
+    }
 }
