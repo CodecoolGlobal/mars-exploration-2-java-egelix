0f79b3ad07bf07cd137c2ae92ea02e28b10d7459
ErydCode <dyre.espana@gmail.com>
2023-05-24 17:04:44 +0200

    initialised Display and Random & removed new Coordinate

--- src/main/java/com/codecool/marsexploration/Application.java
+++ src/main/java/com/codecool/marsexploration/Application.java
@@ -5,17 +5,21 @@ import com.codecool.marsexploration.data.Coordinate;
 import com.codecool.marsexploration.data.SimulationInput;
 import com.codecool.marsexploration.data.SuccessCondition;
 import com.codecool.marsexploration.logic.ExplorationSimulator;
+import com.codecool.marsexploration.ui.Display;
 import com.codecool.marsexploration.utility.ContextGenerator;
+
+import java.util.Random;
 
 public class Application {
     public static void main(String[] args) {
+        Display display = new Display();
+        Random random = new Random();
         SimulationInput input = new SimulationInput(
                 "/exploration-1.map",
-                new Coordinate(12, 12),
                 100,
                 "src/main/resources/exploration-1.log",
                 new SuccessCondition(5,5));
-        ContextGenerator contextGenerator = new ContextGenerator();
+        ContextGenerator contextGenerator = new ContextGenerator(display, random);
         Context context = contextGenerator.generate(input);
         ExplorationSimulator simulator = new ExplorationSimulator(context);
         simulator.simulate();
