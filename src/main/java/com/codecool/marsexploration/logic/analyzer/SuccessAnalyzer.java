af2988a22e9eec16620d1273d2a022a7b18ab99d
Martin Egert <m.egert82@gmail.com>
2023-05-24 13:01:33 +0200

    added record for successConditions, implemented in classes and worked on analyser-classes

--- src/main/java/com/codecool/marsexploration/logic/analyzer/SuccessAnalyzer.java
+++ src/main/java/com/codecool/marsexploration/logic/analyzer/SuccessAnalyzer.java
@@ -8,6 +8,9 @@ import java.util.Optional;
 public class SuccessAnalyzer implements Analyzer{
     @Override
     public Optional<Outcome> analyze(Context context) {
+        int foundMin = 0;
+        int foundWater = 0;
+        Map<>
         return Optional.empty();
     }
 }
