a1883c80c897b2039039f7c2c0d5110fdd9ac45b
ErydCode <dyre.espana@gmail.com>
2023-05-24 17:00:13 +0200

    delete not needed Class MapGenerator.java

--- src/main/java/com/codecool/marsexploration/utility/MapGenerator.java
+++
@@ -1,9 +0,0 @@
-//package com.codecool.marsexploration.utility;
-
-/*public class MapGenerator {
-    public String[][] generate(String mapPath) {
-        //TODO: write map generator method
-        //return new String[][];
-        return null;
-    }
-}*/
