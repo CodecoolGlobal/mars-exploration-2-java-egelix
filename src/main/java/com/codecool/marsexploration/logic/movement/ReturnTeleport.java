6f481c4e72f1060906e702b96857c272cb7cb9e0
ErydCode <dyre.espana@gmail.com>
2023-05-24 16:58:15 +0200

    rework to new Interface implementation add Name for Log

--- src/main/java/com/codecool/marsexploration/logic/movement/ReturnTeleport.java
+++ src/main/java/com/codecool/marsexploration/logic/movement/ReturnTeleport.java
@@ -1,24 +1,20 @@
 package com.codecool.marsexploration.logic.movement;
 
+import com.codecool.marsexploration.data.Context;
 import com.codecool.marsexploration.data.Coordinate;
 import com.codecool.marsexploration.data.rover.Rover;
 
 import java.util.Set;
 
 public class ReturnTeleport implements Move{
-    private final String name;
-
-    public ReturnTeleport(String name) {
-        this.name = name;
-    }
 
     @Override
-    public void move(String[][] map, Coordinate landing, Set<Coordinate> scannedFields, Rover rover) {
-        rover.setPosition(landing);
+    public void move(Context context) {
+        context.getRover().setPosition(context.getLanding());
     }
 
     @Override
     public String getName() {
-        return name;
+        return "Teleport back to Space Ship";
     }
 }
