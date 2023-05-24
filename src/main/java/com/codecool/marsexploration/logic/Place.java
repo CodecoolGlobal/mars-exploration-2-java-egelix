6e2642b585f784d97f201a5369070cfc23c22a5e
ErydCode <dyre.espana@gmail.com>
2023-05-24 23:29:54 +0200

    add serviceClass Place to add Aliens to 2D map

---
+++ src/main/java/com/codecool/marsexploration/logic/Place.java
@@ -0,0 +1,33 @@
+package com.codecool.marsexploration.logic;
+
+import com.codecool.marsexploration.data.Context;
+import com.codecool.marsexploration.data.Coordinate;
+import com.codecool.marsexploration.data.Symbol;
+
+import java.util.Random;
+
+public class Place {
+    private final Random random;
+
+    public Place(Random random) {
+        this.random = random;
+    }
+
+    public void randomAlien(Context context, int amount) {
+        String[][] map = context.getMap();
+        for (int i = 0; i < amount; i++) {
+        Coordinate randomCoordinate = new Coordinate(random.nextInt(map.length), random.nextInt(map.length));
+        String randomMapSymbol = map[randomCoordinate.y()][randomCoordinate.x()];
+        boolean isFieldEmpty = randomMapSymbol.equals(Symbol.EMPTY.getSymbol());
+            do {
+                if (isFieldEmpty) {
+                    map[randomCoordinate.y()][randomCoordinate.x()] = Symbol.ALIEN.getSymbol();
+                } else {
+                    randomCoordinate = new Coordinate(random.nextInt(map.length), random.nextInt(map.length));
+                    randomMapSymbol = map[randomCoordinate.y()][randomCoordinate.x()];
+                    isFieldEmpty = randomMapSymbol.equals(Symbol.EMPTY.getSymbol());
+                }
+            } while (!isFieldEmpty);
+        }
+    }
+}
