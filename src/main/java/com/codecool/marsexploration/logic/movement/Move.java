5064a4ff929d344140d6ce914ff3bebbdc6b2255
ErydCode <dyre.espana@gmail.com>
2023-05-24 16:59:52 +0200

    rework simplify Argument with 1 Argument Context

--- src/main/java/com/codecool/marsexploration/logic/movement/Move.java
+++ src/main/java/com/codecool/marsexploration/logic/movement/Move.java
@@ -1,12 +1,13 @@
 package com.codecool.marsexploration.logic.movement;
 
+import com.codecool.marsexploration.data.Context;
 import com.codecool.marsexploration.data.Coordinate;
 import com.codecool.marsexploration.data.rover.Rover;
 
 import java.util.Set;
 
 public interface Move {
-    void move(String[][] map, Coordinate landing, Set<Coordinate> scannedFields, Rover rover);
+    void move(Context context);
 
     String getName();
 }
