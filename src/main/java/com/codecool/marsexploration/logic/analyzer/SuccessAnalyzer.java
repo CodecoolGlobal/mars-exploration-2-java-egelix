a22cf100056330c1fcb12b6c0820ee05a74327a1
Martin Egert <m.egert82@gmail.com>
2023-05-24 13:12:46 +0200

    working on analyser

--- src/main/java/com/codecool/marsexploration/logic/analyzer/SuccessAnalyzer.java
+++ src/main/java/com/codecool/marsexploration/logic/analyzer/SuccessAnalyzer.java
@@ -1,8 +1,11 @@
 package com.codecool.marsexploration.logic.analyzer;
 
 import com.codecool.marsexploration.data.Context;
+import com.codecool.marsexploration.data.Coordinate;
 import com.codecool.marsexploration.data.Outcome;
+import com.codecool.marsexploration.data.Symbol;
 
+import java.util.Map;
 import java.util.Optional;
 
 public class SuccessAnalyzer implements Analyzer{
@@ -10,7 +13,21 @@ public class SuccessAnalyzer implements Analyzer{
     public Optional<Outcome> analyze(Context context) {
         int foundMin = 0;
         int foundWater = 0;
-        Map<>
+        Map<Coordinate, String> scannedCoordinates = context.getScannedCoordinates();
+        scannedCoordinates.entrySet().stream()
+                .peek(entry -> {
+                    if (entry.getValue() = Symbol.MINERAL.getSymbol()) {
+                        foundMin += 1;
+                    } else if (entry.getValue() = Symbol.WATER.getSymbol())
+                })
+
+
         return Optional.empty();
+    }
+
+    private void updateFoundResoures(Map.Entry<Coordinate, String> entry, int foundMin, int foundWater) {
+        if (entry.getValue() = Symbol.MINERAL.getSymbol()) {
+
+        }
     }
 }
