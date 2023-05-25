5ad8ec743a6cec688f8c25fd3fbe4f7f0baba71b
ErydCode <dyre.espana@gmail.com>
2023-05-25 17:41:51 +0200

    add Methods and ANSI Colors for Display beautifuler

--- src/main/java/com/codecool/marsexploration/ui/Display.java
+++ src/main/java/com/codecool/marsexploration/ui/Display.java
@@ -4,6 +4,10 @@ import java.util.Arrays;
 
 public class Display {
     private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_BLUE = "\u001B[34m";
+    private static final String ANSI_BLUE_BOLD = "\u001B[1;34m";
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_GREEN_BOLD = "\u001B[1;32m";
     private static final String ANSI_DEFAULT = "\u001B[0m";
     private final int displayLength = 72;
 
@@ -18,18 +22,30 @@ public class Display {
         System.out.println();
         String fillWithSpace = " ".repeat((displayLength - title.length()) / 2);
         System.out.println("✧".repeat(displayLength));
-        System.out.println(fillWithSpace + title.toUpperCase() + fillWithSpace);
+        System.out.println(fillWithSpace + ANSI_GREEN_BOLD + title.toUpperCase() + ANSI_DEFAULT + fillWithSpace);
         System.out.println("✧".repeat(displayLength));
     }
 
     public void printSubtitle(String subtitle) {
         String fillWithSpace = " ".repeat((displayLength - subtitle.length()) / 2);
-        System.out.println(fillWithSpace + subtitle.toUpperCase() + fillWithSpace);
+        System.out.println(fillWithSpace + ANSI_GREEN + subtitle.toUpperCase() + ANSI_DEFAULT + fillWithSpace);
         System.out.println("✧".repeat(displayLength));
+    }
+
+    public void inputRequirement(String requirement) {
+        System.out.println(ANSI_BLUE_BOLD + requirement + ANSI_DEFAULT);
+    }
+
+    public void inputMessage() {
+        System.out.print(ANSI_BLUE + "Your input is: " + ANSI_DEFAULT);
     }
 
     public void message(String message) {
         System.out.println(message);
+    }
+
+    public void printLines() {
+        System.out.println("-".repeat(displayLength));
     }
 
     public void printEndLines() {
